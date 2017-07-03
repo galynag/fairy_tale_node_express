cff078e09b8556cd92b5d55ca38d038a85dc1839
galynag <g.alenicheva@gmail.com>
2017-07-03 10:07:48 +0300

    v0.2 scr for images was added

--- myapp/routes/tales.js
+++ myapp/routes/tales.js
@@ -3,10 +3,10 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res)=> {
-    // let title='Tales page';
-    // let src='skazka2.jpg';
-    // let text= 'test text';
-    res.render('tales', {title: 'Tales page',src: 'skazka2.jpg',text: 'test text'});
+    let title='Tales page';
+    let src='images/skazka2.jpg';
+    let text= 'test text';
+    res.render('tales', {title: title, src: src,text: text});
 });
 
 module.exports = router;
